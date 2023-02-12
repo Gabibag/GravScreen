d2af45cf457ca332c37889cce0c4d0963dc92d0b
Gabibag <97263038+Gabibag@users.noreply.github.com>
2023-02-11 18:14:46 -0800

    idfk man

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -8,16 +8,18 @@ public class GameWindow extends JFrame implements KeyListener, ComponentListener
     public int width;
     public int height;
     public World world;
+    public Player p;
     public Vector2 TopLeft = new Vector2(0,0);
     public GameWindow() {
         super();
+        p = new Player(15,15);
         this.getRootPane().setDoubleBuffered(false);
         world = new World();
-        World.collision.add((new Rectangle(50, 50, 50, 50)));
-        World.collision.add((new Rectangle(90, 90, 100, 300)));
+        //World.collision.add((new Rectangle(50, 50, 50, 50)));
+        //World.collision.add((new Rectangle(90, 90, 100, 300)));
         this.setIgnoreRepaint(true);
-        World.collision.add((new Rectangle(20, 50, 100, 300)));
-        World.collision.add((new Rectangle(30, 50, 100, 300)));
+        //World.collision.add((new Rectangle(20, 50, 100, 300)));
+        //World.collision.add((new Rectangle(30, 50, 100, 300)));
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
         width = (int)size.getWidth();
         height = (int)size.getHeight();
@@ -38,52 +40,114 @@ public class GameWindow extends JFrame implements KeyListener, ComponentListener
     }
     @Override
     public void paintComponents(Graphics g){
+        //p.set();
         super.paintComponents(g);
         //g.setColor(Color.white);
         this.getRootPane().paintImmediately(0, 0, this.getWidth(), this.getHeight());
         this.world.Draw(this, g);
+        p.draw(this,g);
+       // repaint(15);
     }
     @Override
     public void keyTyped(KeyEvent e) {
     }
 
-    public void keyPressed(KeyEvent e) {
 
+
+    public void keyPressed(KeyEvent e) {
+        System.out.println(e.getKeyChar());
+        //if(e.getKeyChar() == 'w') //
+        if(e.getKeyChar() == 'a') {p.move(-10);}
+        //if(e.getKeyChar() == 's') //
+        if(e.getKeyChar() == 'd') {p.move(10);}
        // System.out.println("ran");
         int movamtw = width/3;
         int movamth = height/3;
+        int smoothness = 100;
         if(e.getKeyCode() == KeyEvent.VK_UP){
-                if (TopLeft.y-movamth > 0) {
-                    TopLeft.y -= movamth;
-                }else {
-                    TopLeft.y = 0;
-                }
+            if (TopLeft.y - movamth > 0) {//if the top
+                for (int i = 0; i < smoothness; i++) {
+                    TopLeft.y -= movamth / smoothness;
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    this.setLocation(TopLeft.x, TopLeft.y);
+                    //sleep for 0.0001 second
 
-        }
-        if(e.getKeyCode() == KeyEvent.VK_DOWN){
-            if (TopLeft.y+movamth < height - this.getHeight()) {
-                TopLeft.y += movamth;
+                    paintComponents(this.getGraphics());
+                }
             }
             else {
-                TopLeft.y = height - this.getHeight();
+                for (int i = 0; i < TopLeft.y; i++) {
+                    TopLeft.y -= movamth / smoothness;
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    this.setLocation(TopLeft.x, TopLeft.y);
+                    //sleep for 0.0001 second
+
+                    paintComponents(this.getGraphics());
+                }
+                TopLeft.y = 0;
+            }
+        }
+        if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            if (TopLeft.y + movamth < height) {
+                for (int i = 0; i < smoothness; i++) {
+                    TopLeft.y += movamth / smoothness;
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    this.setLocation(TopLeft.x, TopLeft.y);
+                    //sleep for 0.0001 second
+
+                    paintComponents(this.getGraphics());
+                }
+            }
+            else {
+                TopLeft.y = height-movamth;
             }
 
         }
 
         if(e.getKeyCode() == KeyEvent.VK_RIGHT){
-                if (TopLeft.x+movamtw < width - this.getWidth()){
-                        TopLeft.x += movamtw;
-                        //sleep for 0.0001 seconds
+            if (TopLeft.x + movamtw< width-this.getWidth()) {
+                for (int i = 0; i < smoothness; i++) {
+                    TopLeft.x += movamtw / smoothness;
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    this.setLocation(TopLeft.x, TopLeft.y);
+                    //sleep for 0.0001 second
 
-                    TopLeft.x += 1;
-                }else {
-                    TopLeft.x = width - this.getWidth();
+                    paintComponents(this.getGraphics());
                 }
+            }else {
+                TopLeft.x = width-this.getWidth();
+            }
                 //sleep for 0.0001 seconds
         }
         if(e.getKeyCode() == KeyEvent.VK_LEFT){
-            if (TopLeft.x-movamtw > 0) {
-                TopLeft.x -= movamtw;
+            if (TopLeft.x - movamtw > 0) {
+                for (int i = 0; i < smoothness; i++) {
+                    TopLeft.x -= movamtw / smoothness;
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    this.setLocation(TopLeft.x, TopLeft.y);
+                    //sleep for 0.0001 second
+                    paintComponents(this.getGraphics());
+                }
             }else {
                 TopLeft.x = 0;
             }
@@ -96,7 +160,10 @@ public class GameWindow extends JFrame implements KeyListener, ComponentListener
         paintComponents(this.getGraphics());
 
     }
-     public void componentResized(ComponentEvent e) {
+
+
+
+    public void componentResized(ComponentEvent e) {
   }
   public void componentMoved(ComponentEvent e) {
     int x = e.getComponent().getX();
@@ -110,6 +177,10 @@ public class GameWindow extends JFrame implements KeyListener, ComponentListener
   public void componentHidden(ComponentEvent e) {
   }
     public void keyReleased(KeyEvent e) {
+         if(e.getKeyChar() == 'w') p.keyUp = false;
+        if(e.getKeyChar() == 'a') p.keyLeft = false;
+        if(e.getKeyChar() == 's') p.keyDown = false;
+        if(e.getKeyChar() == 'd') p.keyRight = false;
     }
 
 
--- src/Player.java
+++ src/Player.java
@@ -1,12 +1,11 @@
 
 import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
+import java.awt.Graphics;
 
 
 public class Player {
-    double x;
-    double y;
+    public double x;
+    public double y;
     double w;
     double h;
     double xspeed;
@@ -19,54 +18,61 @@ public class Player {
     boolean keyUp;
     boolean keyDown;
 
+    public void move(int i){
+        System.out.println("moving");
+        this.x += i;
+    }
     public Player(int x, int y) {
         this.x = x;
         this.y = y;
-        
-
-        w = 50;
-        h = 60;
-        hitBox = new Rectangle(x,y, (int) w, (int) h);
+        w = 80;
+        h = 90;
+        hitBox = new Rectangle(x, y, (int) w, (int) h);
     }
 
-    public void set(){
-        if(keyLeft && keyRight || !keyLeft && !keyRight) xspeed *= 0.1;
-        if(keyLeft && !keyRight) xspeed--;
-        if(keyRight && !keyLeft) xspeed++;
-        if(xspeed > 0 && xspeed < 0.1) xspeed = 0;
-        if(xspeed < 0 && xspeed > -0.1) xspeed = 0;
+    // public void set() {
+    //     System.out.println(y + "y / x" + x);
+    //     if (keyLeft && keyRight || !keyLeft && !keyRight) xspeed *= 0.1;
+    //     if (keyLeft && !keyRight) xspeed--;
+    //     if (keyRight && !keyLeft) xspeed++;
+    //     if (xspeed > 0 && xspeed < 0.1) xspeed = 0;
+    //     if (xspeed < 0 && xspeed > -0.1) xspeed = 0;
 
-        if(xspeed > 1) xspeed = 0.2;
-        if(xspeed < -1) xspeed = -0.2;
+    //     if (xspeed > 1) xspeed = 0.2;
+    //     if (xspeed < -1) xspeed = -0.2;
 
 
-        if(keyUp && (y > 299 && y < 301)) {
-            y -= 100;
+    //     if (keyUp && (y > 299 && y < 301)) {
+    //         y -= 100;
+    //     }
+    //     keyUp = false;
+
+
+    //     if (y < 300) {
+    //         yspeed = 0.1;
+    //     }
+    //     if (y > 300) yspeed = 0;
+
+
+    //     x += xspeed;
+    //     y += yspeed;
+    //     hitBox.BottomLeft.x = (int) x;
+    //     hitBox.BottomLeft.y = (int) y;
+
+
+    // }
+
+    public void draw(GameWindow w, Graphics g) {
+        Rectangle r = new Rectangle((int)this.x, (int)this.y, (int)this.h, (int)this.w);
+        if (new Rectangle(w.getLocationOnScreen().x, w.getLocationOnScreen().y, w.getWidth(),
+                          w.getHeight()).contains(r)) {
+           // System.out.println("aa");
+            g.setColor(Color.black);
+            //g.drawRect(5,5, 20,20);
+            g.drawRect(r.BottomLeft.x - w.getX(), r.BottomLeft.y - w.getY(), (int) this.w, (int) this.h);
+
         }
-        keyUp = false;
-
-
-            if(y < 300){
-                yspeed = 0.1;
-            }
-            if(y >300) yspeed = 0;
-
-
-
-
-        x += xspeed;
-        y += yspeed;
-        hitBox.x = (int) x;
-        hitBox.y = (int) y;
-
 
     }
-
-    public void draw(Graphics2D gtd){
-        gtd.setColor(Color.CYAN);
-        gtd.fillRect((int) x, (int) y, (int) w, (int) h);
-
-    }
-
 }
 
